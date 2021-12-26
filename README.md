ac9f25f900865dbf0293988c2c57db64b592f17c
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2021-12-26 23:25:14 +0600

    readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Gold Lavender Task Django
  
+## Commands to run in CMD
+### Create Virtual Environment
+#### (Example Command)
+```commandline
+py -m venv venv
+```
+### Activate Virtual Environment
+```commandline
+venv\Scripts\activate
+```
+### Run Project
+```commandline
+py manage.py runserver
+```
--- apiapp/views.py
+++ apiapp/views.py
@@ -1,8 +1,12 @@
+from django.db.models import Q
 from rest_framework import generics, filters, exceptions
 from rest_framework.response import Response
 
 from apiapp import serializers
 from mobilephones import models
+from django.core import serializers as ds
+from django.http import HttpResponse
+from django.forms.models import model_to_dict
 
 
 # Create your views here.
@@ -13,20 +17,38 @@ class PhoneApiView(generics.ListAPIView):
     queryset = models.Phone.objects.all()
     search_fields = ['model_name', 'jan_code']
 
-    def filter_queryset(self, queryset):
-        for backend in list(self.filter_backends):
-            queryset = backend().filter_queryset(self.request, queryset, self)
-        return queryset
+    # def get_queryset(self):
+    #     search = self.request.query_params.get('search')
+    #     filter_qs = models.Phone.objects.filter(Q(model_name__icontains=search) | Q(jan_code__contains=search))
+    #     return filter_qs
+    #
+    # def list(self, request, *args, **kwargs):
+    #     ser = self.get_serializer(self.get_queryset(), many=True)
+    #     responseData = ser.data
+    #     search = self.request.query_params.get('search')
+    #     filter_qs = models.Phone.objects.filter(Q(model_name__icontains=search) | Q(jan_code__contains=search))
+    #     if not filter_qs.exists():
+    #         return Response({'response': 'No Data Found'})
+    #     return Response(responseData)
 
-    def get_queryset(self):
-        return models.Phone.objects.all()
+    # def filter_queryset(self, queryset):
+    #     for backend in list(self.filter_backends):
+    #         queryset = backend().filter_queryset(self.request, queryset, self)
+    #     return queryset
 
-    def get(self, request, *args, **kwargs):
-        filtered_qs = self.filter_queryset(self.get_queryset())
 
-        serializer = serializers.PhoneSerializer(filtered_qs, data=request.data)
-        print(request.data)
-        serializer.is_valid(raise_exception=True)
-        if not filtered_qs.exists():
-            return Response({'response': 'No Data Found'})
-        return Response(serializer.data)
+
+
+
+    # def get_queryset(self):
+    #     return models.Phone.objects.all()
+    #
+    # def get(self, request, *args, **kwargs):
+    #     filtered_qs = self.filter_queryset(self.get_queryset())
+    #
+    #     serializer = serializers.PhoneSerializer(filtered_qs, data=request.data)
+    #     print(request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     if not filtered_qs.exists():
+    #         return Response({'response': 'No Data Found'})
+    #     return Response(serializer.data)
